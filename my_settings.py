7ec8f9ccd6f1fb5e46328b107b1e88c850e9ebf0
SonJi <jhson0507@gmail.com>
2021-10-09 15:54:04 +0900

    home 감정, 등급

--- my_settings.py
+++ my_settings.py
@@ -5,9 +5,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aiconcd',
         'USER': 'root',
-        'PASSWORD': '123456', # 본인 비밀번호 넣기
+        'PASSWORD': '1234qwer', # 본인 비밀번호 넣기
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-SECRET_KEY = 'django-insecure-itm+qww5x)&#4@dw+yfxd@616w=*ogx1ht^#4@wqrq5n_5bw7w' # 본인 프로젝트 시크릿키 넣기
+SECRET_KEY = 'django-insecure-l+6zt=h9z$84q&jj%bhpss+t6=l(v0qa5z(gh4*)q_2710t2b#'
